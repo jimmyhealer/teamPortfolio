4e6668692d113f4ddd7ecb8cc3ce7b908c522c56
jimmyhealer <yahing6066@gmail.com>
2022-07-20 11:39:05 +0800

    debug favicon

--- nuxt.config.js
+++ nuxt.config.js
@@ -20,7 +20,7 @@ export default {
       { hid: "description", name: "description", content: "" },
       { name: "format-detection", content: "telephone=no" },
     ],
-    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.png" }],
+    link: [{ rel: "icon", type: "image/x-icon", href: "/ycystudio-portfolio/favicon.png" }],
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
